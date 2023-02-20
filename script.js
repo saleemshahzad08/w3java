e774f3ee66f7ba01b589eaf51c731aef9d5c6436
saleemshahzad08 <pmaf16e018@gmail.com>
2023-02-20 22:16:32 +0500

    UPDATED

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function pmsg() {
-	return "You can simply check that in bottom right corner of your PC you ASSHOLE";
+function cname() {
+	return "Saleem Shahzad";
+}
+
+function edu() {
+	return "He has done M.Phil Mathematics from UOS back in 2018";
+}
+
+function hideImage() {
+	document.getElementById('profile').style.display='none';
+}
+
+function showImage() {
+	document.getElementById('profile').style.display='block';
 }
